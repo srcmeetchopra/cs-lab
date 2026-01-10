73e5510073517c60dce8a18142b31878a85522c0
Meet Chopra <meetchopra222@gmail.com>
2026-01-10 23:52:25 +0530

    Create test.c

---
+++ C-Prog/New folder/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){/* varables & data types
+           char,1,%c,-128 to 127
+           int,4,%d,"-2,147,483,648 to 2,147,483,647"
+           float,4,%f,1.2×10−38 to 3.4×1038
+           double,8,%lf,2.3×10−308 to 1.7×10308
+           */
+           return 0}
